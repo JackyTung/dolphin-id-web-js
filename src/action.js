0b1cb04f4ccd63f4f1e580abd438192e011f47ff
Alien Lien <alienlien@gmail.com>
2020-06-14 18:35:19 +0800

    [Change] Refactor codes on the change of region
    - Add region change state: resize and move.
    - Introduce region change state to replace unclear isMove.
    - Isolate functions for region moving and resizing.
    - Introduce opposite corner to replace hard-to-read clientStart.
    - Add action and reducer for region changing.
    - Manually test.

--- src/action.js
+++ src/action.js
@@ -1,5 +1,6 @@
 export const SET_TRIP_NUMBER = 'SET_TRIP_NUMBER'
 export const SET_REGIONS = 'SET_REGIONS'
+export const SET_REGION_IS_CHANGING = 'SET_REGION_IS_CHANGING'
 
 export function modifyColor(color, index) {
     return {
@@ -21,5 +22,18 @@ export function setRegions(regions) {
     return {
         type: SET_REGIONS,
         regions: regions
+    }
+}
+
+/**
+ * Returns the action to indicate whether the region is changing or not.
+ * 
+ * @param {*} isChanging Boolean to indicate whether the region is changing or not.
+ * @return {type} - An action indicates whether the region is changing or not.
+ */
+export function setRegionIsChanging(isChanging) {
+    return {
+        type: SET_REGION_IS_CHANGING,
+        isChanging: isChanging
     }
 }
--- src/components/RegionSelect.js
+++ src/components/RegionSelect.js
@@ -3,13 +3,14 @@ import { PropTypes } from 'prop-types';
 import objectAssign from 'object-assign';
 import Region from './Region';
 import style from './style';
+import RegionChangeState from '../const/RegionChangeState';
 
 class RegionSelect extends Component {
 	constructor (props) {
 		super(props);
 		this.onComponentMouseTouchDown = this.onComponentMouseTouchDown.bind(this);
 		this.onDocMouseTouchMove = this.onDocMouseTouchMove.bind(this);
-		this.onDocMouseTouchEnd = this.onDocMouseTouchEnd.bind(this);
+		this.stopChangeRegion = this.stopChangeRegion.bind(this);
 		this.onRegionMoveStart = this.onRegionMoveStart.bind(this);
 		this.regionCounter = 0;
 	}
@@ -17,109 +18,34 @@ class RegionSelect extends Component {
 		document.addEventListener('mousemove', this.onDocMouseTouchMove);
 		document.addEventListener('touchmove', this.onDocMouseTouchMove);
 
-		document.addEventListener('mouseup', this.onDocMouseTouchEnd);
-		document.addEventListener('touchend', this.onDocMouseTouchEnd);
-		document.addEventListener('touchcancel', this.onDocMouseTouchEnd);
+		document.addEventListener('mouseup', this.stopChangeRegion);
+		document.addEventListener('touchend', this.stopChangeRegion);
+		document.addEventListener('touchcancel', this.stopChangeRegion);
 	}
 	componentWillUnmount() {
 		document.removeEventListener('mousemove', this.onDocMouseTouchMove);
 		document.removeEventListener('touchmove', this.onDocMouseTouchMove);
 
-		document.removeEventListener('mouseup', this.onDocMouseTouchEnd);
-		document.removeEventListener('touchend', this.onDocMouseTouchEnd);
-		document.removeEventListener('touchcancel', this.onDocMouseTouchEnd);
+		document.removeEventListener('mouseup', this.stopChangeRegion);
+		document.removeEventListener('touchend', this.stopChangeRegion);
+		document.removeEventListener('touchcancel', this.stopChangeRegion);
 	}
-	getClientPos(e) {
-		let pageX, pageY;
 
-		if (e.touches) {
-			pageX = e.touches[0].pageX;
-			pageY = e.touches[0].pageY;
-		} else {
-			pageX = e.pageX;
-			pageY = e.pageY;
-		}
-
-		return {
-			x: pageX,
-			y: pageY
-		};
-	}
-	onDocMouseTouchMove (event) {
-		if (!this.isChanging) {
-			return;
-		}
-		const index = this.regionChangeIndex;
-		const updatingRegion = this.props.regions[index];
-		const clientPos = this.getClientPos(event);
-		const regionChangeData = this.regionChangeData;
-
-		let x, y, width, height;
-		if (!regionChangeData.isMove) {
-			let x1Pc, y1Pc, x2Pc, y2Pc;
-			x1Pc = (regionChangeData.clientPosXStart - regionChangeData.imageOffsetLeft) / regionChangeData.imageWidth * 100;
-			y1Pc = (regionChangeData.clientPosYStart - regionChangeData.imageOffsetTop) / regionChangeData.imageHeight * 100;
-			x2Pc = (clientPos.x - regionChangeData.imageOffsetLeft) / regionChangeData.imageWidth * 100;
-			y2Pc = (clientPos.y - regionChangeData.imageOffsetTop) / regionChangeData.imageHeight * 100;
-			x = Math.min(x1Pc, x2Pc);
-			y = Math.min(y1Pc, y2Pc);
-			width = Math.abs(x1Pc - x2Pc);
-			height = Math.abs(y1Pc - y2Pc);
-			if(this.props.constraint){
-				if (x2Pc >= 100) { x = x1Pc; width = 100 - x1Pc; }
-				if (y2Pc >= 100) { y = y1Pc; height = 100 - y1Pc; }
-				if (x2Pc <= 0) { x = 0; width = x1Pc; }
-				if (y2Pc <= 0) { y = 0; height = y1Pc; }
-			}
-		} else {
-			x = (clientPos.x + regionChangeData.clientPosXOffset - regionChangeData.imageOffsetLeft) / regionChangeData.imageWidth * 100;
-			y = (clientPos.y + regionChangeData.clientPosYOffset - regionChangeData.imageOffsetTop) / regionChangeData.imageHeight * 100;
-			width = updatingRegion.width;
-			height = updatingRegion.height;
-			if(this.props.constraint){
-				if (x + width >= 100) { x = Math.round(100 - width); }
-				if (y + height >= 100) { y = Math.round(100 - height); }
-				if (x <= 0) { x = 0; }
-				if (y <= 0) { y = 0; }
-			}
-		}
-
-		const rect = {
-			x: x,
-			y: y,
-			width: width,
-			height: height,
-			isChanging: true
-		};
-		this.props.onChange([
-			...this.props.regions.slice(0, index),
-			objectAssign({}, updatingRegion, rect),
-			...this.props.regions.slice(index + 1)
-		]);
-	}
-	onDocMouseTouchEnd () {
-		if (this.isChanging) {
-			this.isChanging = false;
-			const index = this.regionChangeIndex;
-			const updatingRegion = this.props.regions[index];
-			const changes = {
-				new: false,
-				isChanging: false
-			};
-			this.regionChangeIndex = null;
-			this.regionChangeData = null;
-			this.props.onChange([
-				...this.props.regions.slice(0, index),
-				objectAssign({}, updatingRegion, changes),
-				...this.props.regions.slice(index + 1)
-			]);
-		}
-	}
 	onComponentMouseTouchDown (event) {
-		if (event.target.dataset.wrapper || event.target.dataset.dir || isSubElement(event.target, (el) => el.dataset && el.dataset.wrapper)) {
+		if (this.isInExistedRegion(event)) {
 			return;
 		}
 		event.preventDefault();
+		this.createRegion(event)
+	}
+
+	isInExistedRegion(event) {
+		return event.target.dataset.wrapper 
+			|| event.target.dataset.dir 
+			|| isSubElement(event.target, (el) => el.dataset && el.dataset.wrapper)
+	}
+
+	createRegion(event) {
 		const clientPos = this.getClientPos(event);
 		const imageOffset = this.getElementOffset(this.refs.image);
 		const xPc = (clientPos.x - imageOffset.left) / this.refs.image.offsetWidth * 100;
@@ -138,24 +64,42 @@ class RegionSelect extends Component {
 		this.regionChangeData = {
 			imageOffsetLeft: imageOffset.left,
 			imageOffsetTop: imageOffset.top,
-			clientPosXStart: clientPos.x,
-			clientPosYStart: clientPos.y,
 			imageWidth: this.refs.image.offsetWidth,
 			imageHeight: this.refs.image.offsetHeight,
-			isMove: false
+			oppositeCorner: {
+				x: clientPos.x,
+				y: clientPos.y,
+			},
+			changeState: RegionChangeState.Resize,
 		};
-
-		if (this.props.regions.length < this.props.maxRegions) {
-			this.props.onChange(this.props.regions.concat(rect));
-			this.regionChangeIndex = this.props.regions.length;
-		} else {
-			this.props.onChange([
-				...this.props.regions.slice(0, this.props.maxRegions - 1),
-				rect
-			]);
-			this.regionChangeIndex = this.props.maxRegions - 1;
-		}
+		this.props.onChange(this.props.regions.concat(rect));
+		this.regionChangeIndex = this.props.regions.length;
 	}
+
+	/**
+	 * Returns the page x and y positions for event. 
+	 * It supports both the desktop and touchable devices.
+	 * 
+	 * @param {*} e Event. 
+	 * @return {type} - It returns the x and y position for client. 
+	 */
+	getClientPos(e) {
+		let pageX, pageY;
+
+		if (e.touches) {
+			pageX = e.touches[0].pageX;
+			pageY = e.touches[0].pageY;
+		} else {
+			pageX = e.pageX;
+			pageY = e.pageY;
+		}
+
+		return {
+			x: pageX,
+			y: pageY
+		};
+	}
+
 	getElementOffset (el) {
 		const rect = el.getBoundingClientRect();
 		const docEl = document.documentElement;
@@ -168,6 +112,26 @@ class RegionSelect extends Component {
 			left: rectLeft
 		};
 	}
+
+	stopChangeRegion () {
+		if (this.isChanging) {
+			this.isChanging = false;
+			const index = this.regionChangeIndex;
+			const updatingRegion = this.props.regions[index];
+			const changes = {
+				new: false,
+				isChanging: false
+			};
+			this.regionChangeIndex = null;
+			this.regionChangeData = null;
+			this.props.onChange([
+				...this.props.regions.slice(0, index),
+				objectAssign({}, updatingRegion, changes),
+				...this.props.regions.slice(index + 1)
+			]);
+		}
+	}
+
 	onRegionMoveStart (event, index) {
 		if (!event.target.dataset.wrapper && !event.target.dataset.dir) {
 			return;
@@ -177,54 +141,199 @@ class RegionSelect extends Component {
 		const clientPos = this.getClientPos(event);
 		const imageOffset = this.getElementOffset(this.refs.image);
 
-		let clientPosXStart, clientPosYStart;
-
+		// Note that the x, y, width, height of currentRegion are all percentage.
 		const currentRegion = this.props.regions[index];
-		const regionLeft = (currentRegion.x / 100 * this.refs.image.offsetWidth) + imageOffset.left;
-		const regionTop = (currentRegion.y / 100 * this.refs.image.offsetHeight) + imageOffset.top;
-		const regionWidth = (currentRegion.width / 100 * this.refs.image.offsetWidth);
-		const regionHeight = (currentRegion.height / 100 * this.refs.image.offsetHeight);
-		const clientPosDiffX = regionLeft - clientPos.x;
-		const clientPosDiffY = regionTop - clientPos.y;
+		const regionLeft = ((currentRegion.x / 100) * this.refs.image.offsetWidth) + imageOffset.left;
+		const regionTop = ((currentRegion.y / 100) * this.refs.image.offsetHeight) + imageOffset.top;
+		const regionWidth = ((currentRegion.width / 100) * this.refs.image.offsetWidth);
+		const regionHeight = ((currentRegion.height / 100) * this.refs.image.offsetHeight);
+
+		const clientPosXOffset = regionLeft - clientPos.x;
+		const clientPosYOffset = regionTop - clientPos.y;
 
 		const resizeDir = event.target.dataset.dir;
 
-		if (resizeDir) {
-			if (resizeDir === 'se') {
-				clientPosXStart = regionLeft;
-				clientPosYStart = regionTop;
-			} else if (resizeDir === 'sw') {
-				clientPosXStart = regionLeft + regionWidth;
-				clientPosYStart = regionTop;
-			} else if (resizeDir === 'nw') {
-				clientPosXStart = regionLeft + regionWidth;
-				clientPosYStart = regionTop + regionHeight;
-			} else if (resizeDir === 'ne') {
-				clientPosXStart = regionLeft;
-				clientPosYStart = regionTop + regionHeight;
-			}
-		} else {
-			clientPosXStart = clientPos.x;
-			clientPosYStart = clientPos.y;
-		}
-
-
-		this.isChanging = true;
-		this.regionChangeData = {
+		let regionChangeData = {
+			clientPosXOffset: clientPosXOffset,
+			clientPosYOffset: clientPosYOffset,
 			imageOffsetLeft: imageOffset.left,
 			imageOffsetTop: imageOffset.top,
-			clientPosXStart: clientPosXStart,
-			clientPosYStart: clientPosYStart,
-			clientPosXOffset: clientPosDiffX,
-			clientPosYOffset: clientPosDiffY,
 			imageWidth: this.refs.image.offsetWidth,
 			imageHeight: this.refs.image.offsetHeight,
-			isMove: resizeDir ? false : true,
+			changeState: resizeDir? RegionChangeState.Resize : RegionChangeState.Move,
 			resizeDir: resizeDir
 		};
 
+		if (resizeDir) {
+			regionChangeData.oppositeCorner = this.getOppositeCorner(
+				resizeDir,
+				regionLeft,
+				regionTop,
+				regionWidth,
+				regionHeight,
+			)
+		} else {
+			regionChangeData.oldClientPos = clientPos
+		}
+
+		this.isChanging = true;
 		this.regionChangeIndex = index;
+		this.regionChangeData = regionChangeData
 	}
+
+	getOppositeCorner(
+		resizeDir,
+		regionLeft,
+		regionTop,
+		regionWidth,
+		regionHeight) {
+		switch (resizeDir) {
+		    case 'se':
+				return {
+					x: regionLeft,
+					y: regionTop,
+				}
+            case 'sw':
+				return {
+					x: regionLeft + regionWidth,
+					y: regionTop,
+				}
+            case 'nw':
+				return {
+					x: regionLeft + regionWidth,
+					y: regionTop + regionHeight,
+				}
+            case 'ne':
+				return {
+					x: regionLeft,
+					y: regionTop + regionHeight,
+				}
+            default:
+				return {
+					x: regionLeft,
+					y: regionTop,
+				}
+		}
+	}
+
+	onDocMouseTouchMove (event) {
+		if (!this.isChanging) {
+			return;
+		}
+		const index = this.regionChangeIndex;
+		const updatingRegion = this.props.regions[index];
+		const clientPos = this.getClientPos(event);
+		const regionChangeData = this.regionChangeData;
+
+		let region;
+		switch (regionChangeData.changeState) {
+			case RegionChangeState.Resize:
+				region = this.updateRegionResize(
+					clientPos,
+					regionChangeData
+				)
+				break;
+            case RegionChangeState.Move:
+				region = this.updateRegionMove(
+					clientPos,
+					regionChangeData,
+					updatingRegion
+				)
+				break;
+            default:
+				console.log('Invalid state:' + regionChangeData)
+				return;
+		}
+
+		const rect = Object.assign(
+			{}, 
+			region, 
+			{isChanging: true}
+		)
+		this.props.onChange([
+			...this.props.regions.slice(0, index),
+			objectAssign({}, updatingRegion, rect),
+			...this.props.regions.slice(index + 1)
+		]);
+	}
+
+	updateRegionResize(
+		clientPos, 
+		regionChangeData) {
+		let oppositeCornerX = ((regionChangeData.oppositeCorner.x - regionChangeData.imageOffsetLeft) / regionChangeData.imageWidth) * 100;
+		let oppositeCornerY = ((regionChangeData.oppositeCorner.y - regionChangeData.imageOffsetTop) / regionChangeData.imageHeight) * 100;
+		let newCornerX = ((clientPos.x - regionChangeData.imageOffsetLeft) / regionChangeData.imageWidth) * 100;
+		let newCornerY = ((clientPos.y - regionChangeData.imageOffsetTop) / regionChangeData.imageHeight) * 100;
+		let x = Math.min(oppositeCornerX, newCornerX);
+		let y = Math.min(oppositeCornerY, newCornerY);
+		let width = Math.abs(oppositeCornerX - newCornerX);
+		let height = Math.abs(oppositeCornerY - newCornerY);
+
+		// Deal with the case with constraint
+		if(this.props.constraint){
+			if (newCornerX >= 100) { 
+				x = oppositeCornerX; 
+				width = 100 - oppositeCornerX; 
+			}
+			
+			if (newCornerY >= 100) { 
+				y = oppositeCornerY; 
+				height = 100 - oppositeCornerY; 
+			}
+
+			if (newCornerX <= 0) { 
+				x = 0; 
+				width = oppositeCornerX; 
+			}
+			
+			if (newCornerY <= 0) { 
+				y = 0; 
+				height = oppositeCornerY; 
+			}
+		}
+		return {
+			x: x,
+			y: y,
+			width: width,
+			height: height,
+		}
+	}
+
+	updateRegionMove(
+		clientPos, 
+		regionChangeData, 
+		updatingRegion) {
+		let x = (((clientPos.x + regionChangeData.clientPosXOffset) - regionChangeData.imageOffsetLeft) / regionChangeData.imageWidth) * 100;
+		let y = (((clientPos.y + regionChangeData.clientPosYOffset) - regionChangeData.imageOffsetTop) / regionChangeData.imageHeight) * 100;
+		let width = updatingRegion.width;
+		let height = updatingRegion.height;
+
+		// Deal with the case with constraint
+		if(this.props.constraint){
+			if (x + width >= 100) { 
+				x = Math.round(100 - width); 
+			}
+			
+			if (y + height >= 100) { 
+				y = Math.round(100 - height); 
+			}
+			
+			if (x <= 0) { 
+				x = 0; 
+			}
+			
+			if (y <= 0) { 
+				y = 0; 
+			}
+		}
+		return {
+			x: x,
+			y: y,
+			width: width,
+			height: height,
+		}
+	}
+
 	renderRect (rect, index) {
 		return <Region
 			x={rect.x}
@@ -241,12 +350,17 @@ class RegionSelect extends Component {
 			changing={index === this.regionChangeIndex}
 		/>;
 	}
+
 	render () {
 		const regions = this.props.regions;
 		return (
 			<div
 				ref='image'
-				style={objectAssign({}, style.RegionSelect, this.props.style)}
+				style={Object.assign(
+					{}, 
+					style.RegionSelect, 
+					this.props.style
+				)}
 				className={this.props.className}
 				onTouchStart={this.onComponentMouseTouchDown}
 				onMouseDown={this.onComponentMouseTouchDown}>
@@ -272,6 +386,7 @@ class RegionSelect extends Component {
 		);
 	}
 }
+
 RegionSelect.propTypes = {
 	constraint: PropTypes.bool,
 	regions: PropTypes.array,
@@ -282,8 +397,10 @@ RegionSelect.propTypes = {
 	debug: PropTypes.bool,
 	className: PropTypes.string,
 	style: PropTypes.object,
-	regionStyle: PropTypes.object
+	regionStyle: PropTypes.object,
+	isChanging: PropTypes.bool
 };
+
 RegionSelect.defaultProps = {
 	maxRegions: Infinity,
 	debug: false,
